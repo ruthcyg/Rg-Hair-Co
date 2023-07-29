d346f9a0c69de0ef2c59d2048c700786cafb61c9
ruthcyg <ewho.ruth2014@gmail.com>
2023-07-29 18:51:35 +0530

    fist app

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const hairData = [
+  {
+    name: "Curly Hair",
+    ingredients: "Beautiful curly hair",
+    price: 6,
+    photoName: "hairs/hair.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Straight Hair",
+    ingredients: "Beautiful straight hair",
+    price: 10,
+    photoName: "hairs/hair2.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Wave Hair",
+    ingredients: "Grade 10A wave hair",
+    price: 12,
+    photoName: "hairs/hair3.jpg",
+    soldOut: false,
+  },
+  {
+    name: "curl Hair",
+    ingredients: "Handmade curl hair",
+    price: 12,
+    photoName: "hairs/hair4.jpg",
+    soldOut: false,
+  },
+  {
+    name: "wet Hair",
+    ingredients: "wet and wavy hair",
+    price: 15,
+    photoName: "hairs/hair5.jpg",
+    soldOut: true,
+  },
+  {
+    name: "bone Straight Hair",
+    ingredients: "bone straight hair",
+    price: 18,
+    photoName: "hairs/hair6.jpg",
+    soldOut: false,
+  },
+];
+
+
+export default hairData;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,113 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import hairData from "./data.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <OurHair />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>RG Hair Co.</h1>
+    </header>
+  );
+}
+
+function OurHair() {
+  return (
+    <main className="menu">
+      <h2>Our Different Hair</h2>
+
+      <ul className="pizzas">
+        {hairData.map((hair) => (
+          <Hair key={hair.name} hairObjects={hair} />
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+function Hair(props) {
+  console.log(props);
+  return (
+    <li className="pizza">
+      <img src={props.hairObjects.photoName} alt={props.hairObjects.name} />
+      <div>
+      <h3>{props.hairObjects.name}</h3>
+      <p>{props.hairObjects.ingredients}</p>
+      <span>{props.hairObjects.price + 10}</span>
+      </div>
+    </li>
+  );
+}
+
+// function Ourhair() {
+//   return (
+//     <main className="menu">
+//       <h2>Our Different Hair</h2>
+
+//       <div>
+//         {hairData.map((hair )=> (
+
+//        <Hair hairObjects = {hair} />
+
+//         ))}
+//       </div>
+//       <Hair />
+
+{
+  /*<Hair
+        name="Wavy Hair"
+        ingredients="Wavy curly hair"
+        photoName="hairs/hair2.jpg"
+        price={25}
+  />*/
+}
+//     </main>
+//   );
+// }
+
+// function Hair(props) {
+//   console.log(props);
+//   return (
+//     <div className="pizza">
+//       <img src={props.hairObjects.photoName} alt={props.hair0bjects.name} />
+//       <div>
+//         <h3>{props.hairObjects.name}</h3>
+//         <p>{props.hairObjects.ingredients}</p>
+//         <span>{props.hairObjects.price + 10}</span>
+//       </div>
+//     </div>
+//   );
+// }
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open !");
+  // else alert("We're are Closed");
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()} RG Hair Co. 2023
+    </footer>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
