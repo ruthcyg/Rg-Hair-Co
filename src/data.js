b224fc2da72468c4e58e6180ac6aba45fc04fe41
ruthcyg <ewho.ruth2014@gmail.com>
2023-08-02 17:11:08 +0530

    STILL IN PROGRESS

--- src/data.js
+++ src/data.js
@@ -1,42 +1,55 @@
 const hairData = [
   {
-    name: "Curly Hair",
-    ingredients: "Beautiful curly hair",
+    name: "Brown Straight",
+
+    ingredients:
+      "Unveil your elegance with our Brown Straight Hair   - naturally shiny, luxuriously soft,  and impeccably sleek trends.",
+    inches: 12,
     price: 6,
     photoName: "hairs/hair.jpg",
     soldOut: false,
   },
   {
-    name: "Straight Hair",
-    ingredients: "Beautiful straight hair",
+    name: "Remy Wave",
+    ingredients:
+      "Revel in the distinct beauty of our Remy Wave – naturally aligned cuticles promising tangle-free, silky waves that radiate with a mesmerizing luster",
+    inches: 12,
     price: 10,
     photoName: "hairs/hair2.jpg",
     soldOut: false,
   },
   {
-    name: "Wave Hair",
-    ingredients: "Grade 10A wave hair",
+    name: "Weft Wave",
+    ingredients:
+      "Immerse yourself in the allure of our Weft Wave – a versatile, richly textured hair enhancement that exudes natural elegance and effortless style.",
+    inches: 12,
     price: 12,
     photoName: "hairs/hair3.jpg",
     soldOut: false,
   },
   {
-    name: "curl Hair",
-    ingredients: "Handmade curl hair",
+    name: "Soft Curly",
+    ingredients:
+      "Embrace the enchanting charm of our Soft Curly hair – bouncy, touchable curls that offer unmatched volume and a captivatingly playful look.",
+    inches: 12,
     price: 12,
     photoName: "hairs/hair4.jpg",
     soldOut: false,
   },
   {
-    name: "wet Hair",
-    ingredients: "wet and wavy hair",
+    name: "Wet Hair",
+    ingredients:
+      "Experience the dynamic versatility of our Wet Hair - naturally glossy, effortlessly manageable, offering you a fresh, vibrant look that's always in style.",
+    inches: 12,
     price: 15,
     photoName: "hairs/hair5.jpg",
     soldOut: true,
   },
   {
-    name: "bone Straight Hair",
-    ingredients: "bone straight hair",
+    name: "Natural Water Wave",
+    ingredients:
+      "Immerse in the allure of our Natural Water Wave – capturing the rhythm of the ocean in its flowing, effortlessly chic, and organically textured waves",
+    inches: 12,
     price: 18,
     photoName: "hairs/hair6.jpg",
     soldOut: false,
--- src/index.js
+++ src/index.js
@@ -9,99 +9,114 @@ function App() {
       <Header />
       <OurHair />
       <Footer />
+  
     </div>
   );
 }
 
 function Header() {
   return (
+    
+    
     <header className="header">
+    
+    
       <h1>RG Hair Co.</h1>
     </header>
   );
 }
 
 function OurHair() {
+  const hairs = hairData;
+  //const hairs = [];
+  const numHair = hairs.length;
+
   return (
     <main className="menu">
       <h2>Our Different Hair</h2>
 
-      <ul className="pizzas">
-        {hairData.map((hair) => (
-          <Hair key={hair.name} hairObjects={hair} />
-        ))}
-      </ul>
+      {numHair > 0 ? (
+        <>
+          <p>
+            "Embrace the magic of 100% pure Indian hair - Versatile, strong, and
+            silky - It's not just hair, it's your crown!"
+          </p>
+          <ul className="pizzas">
+            {hairs.map((hair) => (
+              <Hair key={hair.name} hairObjects={hair} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p> We're still working on our Hair Products Please come back later</p>
+      )}
     </main>
   );
 }
 
-function Hair(props) {
-  console.log(props);
+function Hair({ hairObjects }) {
+  //console.log(hairObjects);
   return (
-    <li className="pizza">
-      <img src={props.hairObjects.photoName} alt={props.hairObjects.name} />
+    <li className={`pizza ${hairObjects.soldOut ? "sold-out" : ""}`}>
+      <img src={hairObjects.photoName} alt={hairObjects.name} />
       <div>
-      <h3>{props.hairObjects.name}</h3>
-      <p>{props.hairObjects.ingredients}</p>
-      <span>{props.hairObjects.price + 10}</span>
+        <h3>{hairObjects.name}</h3>
+        <p>{hairObjects.ingredients}</p>
+        <span>Inches:{hairObjects.inches}</span>
+        {hairObjects.soldOut ? (
+          <span>Sold Out</span>
+        ) : (
+          <span>${hairObjects.price +10} </span>
+        )}
+        
+         {/* <span>
+            {hairObjects.soldOut ? "Sold Out " : hairObjects.price + 10}
+         </span>*/}
+        
       </div>
     </li>
   );
 }
 
-// function Ourhair() {
-//   return (
-//     <main className="menu">
-//       <h2>Our Different Hair</h2>
-
-//       <div>
-//         {hairData.map((hair )=> (
-
-//        <Hair hairObjects = {hair} />
-
-//         ))}
-//       </div>
-//       <Hair />
-
-{
-  /*<Hair
-        name="Wavy Hair"
-        ingredients="Wavy curly hair"
-        photoName="hairs/hair2.jpg"
-        price={25}
-  />*/
-}
-//     </main>
-//   );
-// }
-
-// function Hair(props) {
-//   console.log(props);
-//   return (
-//     <div className="pizza">
-//       <img src={props.hairObjects.photoName} alt={props.hair0bjects.name} />
-//       <div>
-//         <h3>{props.hairObjects.name}</h3>
-//         <p>{props.hairObjects.ingredients}</p>
-//         <span>{props.hairObjects.price + 10}</span>
-//       </div>
-//     </div>
-//   );
-// }
-
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
-  const closeHour = 22;
+  const openHour = 7;
+  const closeHour = 23;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
   // if (hour >= openHour && hour <= closeHour) alert("We're currently open !");
   // else alert("We're are Closed");
+
+  // if(!isOpen) return (
+  //   <p>
+  //     We're Excited to welcome you between {openHour}:00 and {closeHour}:00
+  //   </p>
+  // );
+
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} RG Hair Co. 2023
+      {/*new Date().toLocaleTimeString()*/}
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're Excited to welcome you between {openHour}:00 and {closeHour}:00
+        </p>
+      )}
     </footer>
+  );
+}
+
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00 Come visit us or order
+        online
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
